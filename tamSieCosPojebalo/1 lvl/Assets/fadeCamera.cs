73dadb992bc8d4b97383d2d8d33a8b3b3b083f57
Thirzen <maksimusbed@gmail.com>
2014-11-28 01:43:25 +0100

    +faderDoKamery

---
+++ tamSieCosPojebalo/1 lvl/Assets/fadeCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fadeCamera : MonoBehaviour {
+	public float fadeSpeed = 0.00000001f;   
+
+	void Awake ()
+	{
+		// Set the texture so that it is the the size of the screen and covers it.
+		//guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		guiTexture.color = Color.Lerp(guiTexture.color, Color.clear, fadeSpeed * Time.deltaTime);	}
+}
