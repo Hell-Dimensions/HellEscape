4a2380da82f4569f6df63ca949ce4f92d547dc83
Thirzen <maksimusbed@gmail.com>
2014-12-15 21:46:35 +0100

    +tp
    dodałem możliwość teleportacji, narazie się nie przyda.

---
+++ tamSieCosPojebalo/1 lvl/Assets/flash.cs
@@ -0,0 +1,41 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class flash : MonoBehaviour {
+//	private Vector3 portalNormal;
+//	private flash otherPortal;
+//	private GameObject player;
+//	private CharacterMotor cm;
+//	private CharacterController cc;
+//
+//
+//	// Use this for initialization
+//	void Start () {
+//		cm = GameObject.FindObjectOfType<CharacterMotor>();
+//		cc = GameObject.FindObjectOfType<CharacterController>();
+//		player = cc.gameObject;
+//		//SetOtherPortal();
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+//
+//	public void MovePlayerToThisPortal ()
+//	{
+//		// ustawianie velocity
+//		Vector3 exitVelocity = portalNormal * cc.velocity.magnitude;
+//		cm.SetVelocity(exitVelocity);
+//		// ustawianie gdzie ma się pojawić player
+//		Vector3 exitPosition = transform.position + otherPortal.portalNormal * 2;
+//
+//	}
+//
+//	public void MovePortal (RaycastHit raycastHit)
+//	{		 
+//		transform.position = raycastHit.point;
+//		portalNormal = raycastHit.normal;
+//	}
+//}
---
+++ tamSieCosPojebalo/1 lvl/Assets/portalGun.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class portalGun : MonoBehaviour {
+	public float gunRange = 1000;
+	public Transform portal;
+
+
+	private Vector3 portalNormal;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		//portals = GameObject.FindObjectOfType<flash>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetMouseButtonDown(0))
+		{
+			Vector3 position = Camera.main.transform.position;
+			RaycastHit raycastHit = new RaycastHit();
+			if (Physics.Linecast (position, position +Camera.main.transform.forward * gunRange, out raycastHit, 1 ))
+			{
+				MovePortal(raycastHit);
+			}
+		}
+	}
+
+	public void MovePortal(RaycastHit raycastHit)
+	{
+		portalNormal = raycastHit.normal;
+		transform.position = raycastHit.point;
+		Vector3 exitPosition = transform.position + portalNormal * 2;
+		player.transform.position = exitPosition;
+	}
+}
